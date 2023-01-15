8fdf6614f92bac348e38f263e837a36e89a96bd5
Lauren Waller <laurenhamann3@gmail.com>
2023-01-15 10:26:06 -0600

    If conditional fix

--- src/components/reviewed.js
+++ src/components/reviewed.js
@@ -8,8 +8,14 @@ const Review = ({ review }) => {
         h2: 'Typing'
     }
     const html = `<h2>${div.h2}</h2><span>${div.dot}</span><span>${div.dot}</span><span>${div.dot}</span>`;
+    let isReviewed;
+    if(review){
+        isReviewed = `<p></p>`
+    }else {
+        isReviewed = html;
+    }
     return (
-        <div id="isReviewed" dangerouslySetInnerHTML={{ __html: html }}></div>
+        <div id="isReviewed" dangerouslySetInnerHTML={{ __html: isReviewed }}></div>
     )
 }
 
