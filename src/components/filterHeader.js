4e5e48287eefff167ecbea6a06c99888d10a6cba
Lauren Waller <laurenhamann3@gmail.com>
2023-04-10 11:42:12 -0500

    remove narrators

--- src/components/filterHeader.js
+++ src/components/filterHeader.js
@@ -1,11 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
-import useNarrators from "../hooks/use-narrators"
 
 const FilterHeader = ({ query, aside }) => {
-  const pages = useNarrators()
   let header
-  let slug
   if (query === "emptyQuery" || query === "none") {
     header = ""
     return
@@ -13,23 +9,7 @@ const FilterHeader = ({ query, aside }) => {
     header = `My Top 5 - ${query}`
     return header
   } else {
-    pages.map(page => {
-      if (page.name === query) {
-        console.log(page.name)
-        slug = page.slug
-        header = (
-          <h1>
-            Filter: <Link to={page.slug}>{query}</Link>
-          </h1>
-        )
-      } else {
-        header = (
-          <h1>
-            Filter: <Link to={slug}>{query}</Link>
-          </h1>
-        )
-      }
-    })
+    header = `Filter: ${query}`
     return header
   }
   return <h1>{header}</h1>
